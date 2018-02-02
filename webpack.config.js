7808130f77412f676269d848283664af55d735f7
Ed Ilyin <ed.il@live.com>
2018-02-02 06:57:25 +0200

    adds paket

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./src/index.jsx",
     output: {
         filename: "bundle.js",
         path: __dirname + "/dist"
@@ -10,13 +10,24 @@ module.exports = {
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".webpack.js", ".web.js", ".js"]
+        extensions: [".webpack.js", ".web.js", ".js", ".jsx"]
     },
 
     module: {
+
         loaders: [
+            // {
+            //     test: /\.fs(x|proj)?$/,
+            //     use: {
+            //         loader: "fable-loader",
+            //         options: {
+            //             babel: babelOptions,
+            //             define: isProduction ? [] : ["DEBUG"]
+            //         }
+            //     }
+            // },
             // All files with a '.js' or '.jsx' extension will be handled by 'babel-loader'.
-            {test: /\.jsx??/, loader: 'babel-loader'},
+            { test: /\.jsx?$/, loader: 'babel-loader' },
         ]
     },
 };
